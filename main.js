6969df15a0a3695940a9e0e27ad8f36c762ab1d5
AlmiraPutriW <114887128+AlmiraPutriW@users.noreply.github.com>
2024-09-30 10:14:58 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function welcome() {
+  alert('Sim salabim muncullah elemen-elemen HTML!');
+  const contents = document.querySelector('.contents');
+  contents.removeAttribute('hidden');
+}
+// document.body.onload = welcome;
+
+function increment(){
+  document.getElementById('count').innerText++;
+
+  if (document.getElementById('count').innerText == 7) {
+    const hiddenMessage = document.createElement('h4');
+    hiddenMessage.innerText = 'Selamat! Anda menemukan hadiah tersembunyi...';
+    const image = document.createElement('img');
+    image.setAttribute(
+      'src',
+      'https://raw.githubusercontent.com/dicodingacademy/a315-web-pemula-labs/shared-files/catto.jpg',
+    );
+    const contents = document.querySelector('.contents');
+    contents.appendChild(hiddenMessage);
+    contents.appendChild(image);
+  }
+}
+
+// document.getElementById('incrementButton').onclick = increment;
